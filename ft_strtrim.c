c1739385b041ea776d2a62e409e3735046a940e0
Mustafa Kemal Aktas <muaktas@k1m02s04.42istanbul.com.tr>
2026-01-17 21:30:16 +0300

    strtrim first implementation

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,32 +6,50 @@
 /*   By: muaktas <muaktas@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:57:28 by muaktas           #+#    #+#             */
-/*   Updated: 2026/01/17 15:38:11 by muaktas          ###   ########.fr       */
+/*   Updated: 2026/01/17 21:27:52 by muaktas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-// char	*ft_strtrim(char const *s1, char const *set)
-// {
-// 	char	*new_string;
-// 	size_t	new_string_len;
-// 	size_t	i;
-// 	size_t	j;
-
-// 	new_string_len = ft_strlen(s1) + 1;
-// 	new_string = (char *)malloc(new_string_len);
-// 	if (new_string == NULL)
-// 	{
-// 		return (NULL);
-// 	}
-// 	i = 0;
-// 	while (s1[i] != '\0')
-// 	{
-
-// 	}
+static void	ft_index_finder(char const *s1, char const *set)
+{
+	size_t	start_index;
+	size_t	last_index;
 
 
-// 	return (new_string);
-// }
+
+
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*new_string;
+	size_t	string_len;
+	size_t	start_index;
+	size_t	j;
+	size_t	set_last;
+
+	j = 0;
+	start_index = 0;
+	string_len = ft_strlen(s1);
+	set_last = ft_strlen(set);
+	new_string = (char *)malloc(string_len + 1);
+	if (new_string == NULL)
+		return (NULL);
+	while (set[j] && s1 && s1[start_index] == set[j])
+	{
+		start_index++;
+		j++;
+	}
+	while (s1 && s1[string_len - 1] == set[set_last -1])
+	{
+		string_len--;
+		set_last--;
+	}
+	ft_strlcpy(new_string, &s1[start_index], string_len - start_index + 2);
+	return (new_string);
+}
+
+
